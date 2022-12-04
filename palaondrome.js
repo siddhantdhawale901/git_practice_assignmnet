c232b4a458fa0792d70ed377783281d4c771ba3a
unknown <shreyanksupare140499@gmail.com>
2022-12-04 19:30:17 +0530

    check done

--- palaondrome.js
+++ palaondrome.js
@@ -1,1 +1,16 @@
 let num= 1221
+let bag = "";{
+    for(i=num[0];i>num.length;i--){
+        bag += num[i] 
+    }
+    
+        if([i]==bag)
+        {
+          console.log("Yes")
+        }
+         else 
+        {
+        console.log("No")
+        }
+
+}
--- primenumber.js
+++ primenumber.js
@@ -1,1 +1,14 @@
-let num= 23
+let num= 23;
+let count=0;
+    for(let a=0;a<=num;a++){
+        if(num%a===0)
+            count++
+        }
+        if(count==2)
+        {
+        console.log("Yes")
+        }
+        else
+        {
+        console.log("No")
+        }
